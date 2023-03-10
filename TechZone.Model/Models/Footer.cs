56c3c8204c6d3b2e254d465e49b4675bcd84dfc2
tranvn <tranvanngocyt@gmail.com>
2023-03-10 21:33:21 +0700

    add domain entities for project

---
+++ TechZone.Model/Models/Footer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TechZone.Model.Models
+{
+    [Table("Footers")]
+    public class Footer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [MaxLength(50)]
+        public string ID { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+    }
+}
