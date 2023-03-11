b1f37195dc5ef9564c36accfcdff393a13417a3f
tranvn <tranvanngocyt@gmail.com>
2023-03-11 22:45:14 +0700

    add unit test for repository and service

---
+++ TechZone.UnitTest/RepositoryTest/PostCategoryRepositoryTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using TechZone.Data.Infracstructure;
+using TechZone.Data.Infrastructure;
+using TechZone.Data.Repositories;
+using TechZone.Model.Models;
+
+namespace TechZone.UnitTest.RepositoryTest
+{
+    [TestClass]
+    public class PostCategoryRepositoryTest
+    {
+        private IDbFactory dbFactory;
+        private IPostCategoryRepository objRepository;
+        private IUnitOfWork unitOfWork;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            dbFactory = new DbFactory();
+            objRepository = new PostCategoryRepository(dbFactory);
+            unitOfWork = new UnitOfWork(dbFactory);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_GetAll()
+        {
+            var list = objRepository.GetAll().ToList();
+            Assert.AreEqual(2, list.Count);
+        }
+
+        [TestMethod]
+        public void PostCategory_Repository_Create()
+        {
+            PostCategory category = new PostCategory();
+            category.Name = "Test category";
+            category.Alias = "Test-category";
+            category.Status = true;
+
+            var result = objRepository.Add(category);
+            unitOfWork.Commit();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(2, result.ID);
+        }
+    }
+}
