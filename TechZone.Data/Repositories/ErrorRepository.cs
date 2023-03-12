963101d55d1c54481fb095e610279fc714e4018b
tranvn <tranvanngocyt@gmail.com>
2023-03-12 10:40:37 +0700

    add web api

---
+++ TechZone.Data/Repositories/ErrorRepository.cs
@@ -0,0 +1,16 @@
+﻿using TechZone.Data.Infrastructure;
+using TechZone.Model.Models;
+
+namespace TechZone.Data.Repositories
+{
+    public interface IErrorRepository : IRepository<Error>
+    {
+    }
+
+    public class ErrorRepository : RepositoryBase<Error>, IErrorRepository
+    {
+        public ErrorRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
