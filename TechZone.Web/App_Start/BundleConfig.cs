28dd71dcb5abbd87fcd921ad7982c39f675b0417
tranvn <tranvanngocyt@gmail.com>
2023-03-14 21:21:36 +0700

    route implement for app

--- TechZone.Web/App_Start/BundleConfig.cs
+++ TechZone.Web/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace TechZone.Web
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            /*bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -24,7 +24,7 @@ namespace TechZone.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css"));*/
         }
     }
 }
---
+++ TechZone.Web/app/shared/modules/techzone.common.js
@@ -0,0 +1,3 @@
+﻿(function () {
+    angular.module('techzone.common', ['ui.router'])
+})();
