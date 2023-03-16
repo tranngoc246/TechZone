f35bf7aaa1f4de83246b910a2f3aa2d0503cd564
tranvn <tranvanngocyt@gmail.com>
2023-03-16 14:06:59 +0700

    create form add data

--- TechZone.Web/Api/ProductCategoryController.cs
+++ TechZone.Web/Api/ProductCategoryController.cs
@@ -9,6 +9,7 @@ using System.Web.Http;
 using TechZone.Model.Models;
 using TechZone.Service;
 using TechZone.Web.Infrastructure.Core;
+using TechZone.Web.Infrastructure.Extensions;
 using TechZone.Web.Mappings;
 using TechZone.Web.Models;
 
@@ -27,7 +28,17 @@ namespace TechZone.Web.Api
             this._mappingService = mappingService;
         }
 
+        [Route("getallparents")]
+        [HttpGet]
+        public IHttpActionResult GetAllParents()
+        {
+            var model = _productCategoryService.GetAll();
+            var responseData = _mappingService.Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+            return Ok(responseData);
+        }
+
         [Route("getall")]
+        [HttpGet]
         public IHttpActionResult GetAll(string keyword, int page = 0, int pageSize = 20)
         {
             int totalRow = 0;
@@ -46,6 +57,35 @@ namespace TechZone.Web.Api
                 TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
             };
             return Ok(paginationSet);
+        }
+
+
+        [Route("create")]
+        [HttpPost]
+        [AllowAnonymous]
+        public HttpResponseMessage Create(HttpRequestMessage request, ProductCategoryViewModel productCategoryVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var newProductCategory = new ProductCategory();
+                    newProductCategory.UpdateProductCategory(productCategoryVm);
+
+                    _productCategoryService.Add(newProductCategory);
+                    _productCategoryService.Save();
+
+                    var responseData = _mappingService.Mapper.Map<ProductCategory, ProductCategoryViewModel>(newProductCategory);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
         }
     }
 }
--- TechZone.Web/Models/ProductCategoryViewModel.cs
+++ TechZone.Web/Models/ProductCategoryViewModel.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TechZone.Web.Models
 {
     public class ProductCategoryViewModel
     {
         public int ID { set; get; }
+        [Required]
         public string Name { set; get; }
+        [Required]
         public string Alias { set; get; }
         public string Description { set; get; }
         public int? ParentID { set; get; }
@@ -20,6 +23,7 @@ namespace TechZone.Web.Models
         public string UpdatedBy { set; get; }
         public string MetaKeyword { set; get; }
         public string MetaDescription { set; get; }
+        [Required]
         public bool Status { set; get; }
     }
 }
