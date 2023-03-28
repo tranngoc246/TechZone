5ae2cd73cbb2e28bc578b15329fde240e458e639
tranvn <tranvanngocyt@gmail.com>
2023-03-28 13:46:13 +0700

    intergrate ckeditor and ckfinder

---
+++ TechZone.Web/Api/ProductController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+using TechZone.Model.Models;
+using TechZone.Service;
+using TechZone.Web.Infrastructure.Core;
+using TechZone.Web.Infrastructure.Extensions;
+using TechZone.Web.Mappings;
+using TechZone.Web.Models;
+
+namespace TechZone.Web.Api
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiControllerBase
+    {
+        #region Initialize
+        private readonly IProductService _productService;
+        private readonly IMappingService _mappingService;
+
+        public ProductController(IErrorService errorService, IProductService productService, IMappingService mappingService)
+            : base(errorService)
+        {
+            this._productService = productService;
+            this._mappingService = mappingService;
+        }
+
+        #endregion
+
+        [Route("getallparents")]
+        [HttpGet]
+        public IHttpActionResult GetAllParents()
+        {
+            var model = _productService.GetAll();
+            var responseData = _mappingService.Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(model);
+            return Ok(responseData);
+        }
+
+        [Route("getbyid/{id:int}")]
+        [HttpGet]
+        public IHttpActionResult GetById(int id)
+        {
+            var model = _productService.GetById(id);
+            var responseData = _mappingService.Mapper.Map<Product, ProductViewModel>(model);
+            return Ok(responseData);
+        }
+
+        [Route("getall")]
+        [HttpGet]
+        public IHttpActionResult GetAll(string keyword, int page = 0, int pageSize = 20)
+        {
+            int totalRow = 0;
+            var model = _productService.GetAll(keyword);
+
+            totalRow = model.Count();
+            var query = model.OrderByDescending(x => x.CreatedDate).Skip((page) * pageSize).Take(pageSize);
+
+            var responseData = _mappingService.Mapper.Map<IEnumerable<Product>, IEnumerable<ProductViewModel>>(query);
+
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = responseData,
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+            };
+            return Ok(paginationSet);
+        }
+
+        [Route("create")]
+        [HttpPost]
+        [AllowAnonymous]
+        public HttpResponseMessage Create(HttpRequestMessage request, ProductViewModel productVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var newProduct = new Product();
+                    newProduct.UpdateProduct(productVm);
+                    newProduct.CreatedDate = DateTime.Now;
+                    _productService.Add(newProduct);
+                    _productService.Save();
+
+                    var responseData = _mappingService.Mapper.Map<Product, ProductViewModel>(newProduct);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
+        [Route("update")]
+        [HttpPut]
+        [AllowAnonymous]
+        public HttpResponseMessage Update(HttpRequestMessage request, ProductViewModel productVm)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var dbProduct = _productService.GetById(productVm.ID);
+
+                    dbProduct.UpdateProduct(productVm);
+                    dbProduct.UpdatedDate = DateTime.Now;
+
+                    _productService.Update(dbProduct);
+                    _productService.Save();
+
+                    var responseData = _mappingService.Mapper.Map<Product, ProductViewModel>(dbProduct);
+                    response = request.CreateResponse(HttpStatusCode.Created, responseData);
+                }
+
+                return response;
+            });
+        }
+
+        [Route("delete")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage Delete(HttpRequestMessage request, int id)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var oldProduct = _productService.Delete(id);
+                    _productService.Save();
+
+                    var responseData = _mappingService.Mapper.Map<Product, ProductViewModel>(oldProduct);
+                    response = request.CreateResponse(HttpStatusCode.OK, responseData);
+                }
+
+                return response;
+            });
+        }
+
+        [Route("deletemulti")]
+        [HttpDelete]
+        [AllowAnonymous]
+        public HttpResponseMessage DeleteMulti(HttpRequestMessage request, string checkedProducts)
+        {
+            return CreateHttpResponse(request, () =>
+            {
+                HttpResponseMessage response = null;
+                if (!ModelState.IsValid)
+                {
+                    response = request.CreateResponse(HttpStatusCode.BadRequest, ModelState);
+                }
+                else
+                {
+                    var listProduct = new JavaScriptSerializer().Deserialize<List<int>>(checkedProducts);
+                    foreach (var item in listProduct)
+                    {
+                        _productService.Delete(item);
+                    }
+                    _productService.Save();
+
+                    response = request.CreateResponse(HttpStatusCode.OK, true);
+                }
+
+                return response;
+            });
+        }
+    }
+}
--- TechZone.Web/app/components/products/productListController.js
+++ TechZone.Web/app/components/products/productListController.js
@@ -1,7 +1,129 @@
 ﻿(function (app) {
     app.controller('productListController', productListController);
 
-    function productListController() {
+    productListController.$inject = ['$scope', 'apiService', 'notificationService', '$ngBootbox', '$filter'];
 
+    function productListController($scope, apiService, notificationService, $ngBootbox, $filter) {
+        $scope.products = [];
+        $scope.page = 0;
+        $scope.pagesCount = 0;
+        $scope.getProducts = getProducts;
+        $scope.keyword = '';
+
+        $scope.search = search;
+
+        $scope.deleteProduct = deleteProduct;
+
+        $scope.selectAll = selectAll;
+
+        $scope.isAll = false;
+        function selectAll() {
+            if ($scope.isAll === false) {
+                angular.forEach($scope.products, function (item) {
+                    item.checked = true;
+                });
+                $scope.isAll = true;
+            } else {
+                angular.forEach($scope.products, function (item) {
+                    item.checked = false;
+                });
+                $scope.isAll = false;
+            }
+        }
+
+        $scope.deleteMultiple = deleteMultiple;
+
+        function deleteMultiple() {
+            var listId = [];
+            $.each($scope.selected, function (i, item) {
+                listId.push(item.ID);
+            });
+            var config = {
+                params: {
+                    checkedProducts: JSON.stringify(listId)
+                }
+            }
+            apiService.del('api/product/deletemulti', config, function (result) {
+                notificationService.displaySuccess('Xóa thành công ' + $scope.selected.length + ' bản ghi.');
+                getProducts(checkSearch);
+            }, function (error) {
+                notificationService.displayError('Xóa không thành công');
+            });
+        }
+
+        $scope.$watch("products", function (n, o) {
+            var checked = $filter("filter")(n, { checked: true });
+            if (checked.length) {
+                $scope.selected = checked;
+                $('#btnDelete').removeAttr('disabled');
+            } else {
+                $('#btnDelete').attr('disabled', 'disabled');
+            }
+        }, true);
+
+        function deleteProduct(id) {
+            $ngBootbox.confirm({
+                size: 'small',
+                title: '<b>Xác nhận xóa</b>',
+                message: 'Bạn có chắc chắn muốn xóa không?',
+                buttons: {
+                    confirm: {
+                        label: 'Xóa',
+                        className: 'btn-danger btn-sm'
+                    },
+                    cancel: {
+                        label: 'Hủy',
+                        className: 'btn-secondary btn-sm'
+                    }
+                }
+            }).then(function () {
+                var config = {
+                    params: {
+                        id: id
+                    }
+                }
+                apiService.del('api/product/delete', config, function () {
+                    notificationService.displaySuccess('Xóa thành công!');
+                    $scope.getProducts(checkSearch);
+                }, function () {
+                    notificationService.displayError('Xóa không thành công!');
+                });
+            });
+        }
+
+        var checkSearch = false;
+
+        function search() {
+            checkSearch = true;
+            getProducts(checkSearch);
+        }
+        function getProducts(check, page) {
+            page = page || 0;
+            var config = {
+                params: {
+                    keyword: $scope.keyword,
+                    page: page,
+                    pageSize: 20
+                }
+            }
+            apiService.get('/api/product/getall', config, function (result) {
+                if (check) {
+                    if (result.data.TotalCount == 0) {
+                        notificationService.displayWarning('Không có bản ghi nào được tìm thấy.');
+                    }
+                    else {
+                        notificationService.displaySuccess('Đã tìm thấy ' + result.data.TotalCount + ' bản ghi.');
+                    }
+                }
+                $scope.products = result.data.Items;
+                $scope.page = result.data.Page;
+                $scope.pagesCount = result.data.TotalPages;
+                $scope.totalCount = result.data.TotalCount;
+            }, function () {
+                console.log('Load product failed.');
+            });
+        }
+
+        $scope.getProducts(checkSearch);
     }
 })(angular.module('techzone.products'));
