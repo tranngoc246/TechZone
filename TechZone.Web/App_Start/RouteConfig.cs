877b526267d87cb773ee379bece055eaf0c7e216
tranvn <tranvanngocyt@gmail.com>
2023-04-03 08:49:41 +0700

    sort and search for product

--- TechZone.Web/App_Start/RouteConfig.cs
+++ TechZone.Web/App_Start/RouteConfig.cs
@@ -9,6 +9,11 @@ namespace TechZone.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
+         name: "Search",
+         url: "tim-kiem.html",
+         defaults: new { controller = "Product", action = "Search", id = UrlParameter.Optional },
+         namespaces: new string[] { "TechZone.Web.Controllers" }
+     );routes.MapRoute(
          name: "Login",
          url: "dang-nhap.html",
          defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional },
